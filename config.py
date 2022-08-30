5688f71569b2c3072196629dba40b512bd939d94
Jesus-NewBIe <71976195+Jesus-NewBIe@users.noreply.github.com>
2022-08-30 10:45:17 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cqz2ibusxyoo.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "keekaiyang-employee"
+customregion = "us-east-1"
